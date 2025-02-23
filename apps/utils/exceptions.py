1471b29b2aa766feac3c0fb0abe2f9edc81c397b
Jaewoo Kil <wodn5515@gmail.com>
2025-02-23 19:34:45 +0900

    에러 핸들링 로직 추가

---
+++ apps/utils/exceptions.py
@@ -0,0 +1,39 @@
+from rest_framework.exceptions import APIException
+from rest_framework import status
+from django.utils.translation import gettext_lazy as _
+
+
+class BadRequest(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _("올바르지 않은 요청입니다.")
+    default_code = "BAD_REQUEST"
+
+
+class AuthenticationFailed(APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = _("이메일 또는 비밀번호가 올바르지 않습니다.")
+    default_code = "AUTHENTICATED_FAILED"
+
+
+class NotAuthenticated(APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = _("로그인이 필요합니다.")
+    default_code = "NOT_AUTHENTICATED"
+
+
+class PermissionDenied(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = _("권한이 없습니다.")
+    default_code = "PERMISSION_DENIED"
+
+
+class NotFound(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = _("요청한 데이터를 찾을 수 없습니다.")
+    default_code = "NOT_FOUND"
+
+
+class MethodNotAllowed(APIException):
+    status_code = status.HTTP_405_METHOD_NOT_ALLOWED
+    default_detail = _("허용되지 않는 Method입니다.")
+    default_code = "METHOD_NOT_ALLOWED"
